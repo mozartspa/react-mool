e60d7ccbb656afcf34adfee63b099b33b2babf21
Filippo Dossena <filippo.dossena@mozart.it>
2021-08-23 17:49:34 +0200

    Add detail components

---
+++ packages/eui/src/detail/DetailHeader.tsx
@@ -0,0 +1,51 @@
+import { EuiHorizontalRule, EuiPageHeader, IconType } from "@elastic/eui"
+import {
+  useGetRecordName,
+  useRecord,
+  useResource,
+  useResourceDefinition,
+} from "@react-mool/core"
+import { ReactNode } from "react"
+
+export type DetailHeaderProps = {
+  resource?: string
+  title?: ReactNode
+  icon?: IconType
+  description?: ReactNode
+  actions?: ReactNode[]
+  showIcon?: boolean
+  showHorizontalRule?: boolean
+  children?: ReactNode
+}
+
+export const DetailHeader = (props: DetailHeaderProps) => {
+  const resource = useResource(props.resource)
+  const definition = useResourceDefinition(resource)
+  const getName = useGetRecordName(resource)
+  const record = useRecord()
+
+  const {
+    title = record ? getName(record) : "",
+    icon = definition.icon ?? "list",
+    description,
+    actions,
+    showIcon = false,
+    showHorizontalRule = true,
+    children,
+  } = props
+
+  return (
+    <>
+      <EuiPageHeader
+        pageTitle={title}
+        iconType={showIcon ? icon : undefined}
+        description={description}
+        rightSideItems={actions}
+        rightSideGroupProps={{ gutterSize: "s" }}
+      >
+        {children}
+      </EuiPageHeader>
+      {showHorizontalRule && <EuiHorizontalRule />}
+    </>
+  )
+}
