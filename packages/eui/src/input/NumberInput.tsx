13a0611d6b050600fdfcfc64ae42238208c81ddc
Filippo Dossena <filippo.dossena@mozart.it>
2021-08-26 10:21:04 +0200

    Fix invalid number input when using decimals (on Safari)

--- packages/eui/src/input/NumberInput.tsx
+++ packages/eui/src/input/NumberInput.tsx
@@ -4,5 +4,7 @@ import { Input, InputProps } from "./Input"
 export type NumberInputProps = InputProps & EuiFieldNumberProps
 
 export const NumberInput = (props: NumberInputProps) => (
-  <Input {...props}>{(field) => <EuiFieldNumber {...field.input} {...props} />}</Input>
+  <Input {...props}>
+    {(field) => <EuiFieldNumber {...field.input} step="any" {...props} />}
+  </Input>
 )
