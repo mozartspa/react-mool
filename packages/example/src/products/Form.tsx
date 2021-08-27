c109f8cbd71a1448f3c2956572bff52e7852da00
Filippo Dossena <filippo.dossena@mozart.it>
2021-08-27 10:55:42 +0200

    Update example

---
+++ packages/example/src/products/Form.tsx
@@ -0,0 +1,41 @@
+import { EuiSpacer } from "@elastic/eui"
+import { ShowForCrud } from "@react-mool/core"
+import {
+  EditButton,
+  NumberControl,
+  Row,
+  SaveButton,
+  TextAreaControl,
+  TextControl,
+} from "@react-mool/eui"
+
+export const Form = () => {
+  return (
+    <div style={{ maxWidth: 900 }}>
+      <Row>
+        <TextControl name="reference" />
+        <TextControl name="category_id" />
+      </Row>
+      <Row spacer>
+        <NumberControl name="price" />
+        <NumberControl name="stock" />
+      </Row>
+      <Row spacer>
+        <TextControl name="image" />
+        <TextControl name="thumbnail" />
+      </Row>
+      <Row spacer>
+        <NumberControl name="height" />
+        <NumberControl name="width" />
+      </Row>
+      <TextAreaControl name="description" fullWidth />
+      <EuiSpacer />
+      <ShowForCrud modes={["create", "edit"]}>
+        <SaveButton />
+      </ShowForCrud>
+      <ShowForCrud modes={["detail"]}>
+        <EditButton />
+      </ShowForCrud>
+    </div>
+  )
+}
--- packages/example/src/products/ProductDetail.tsx
+++ packages/example/src/products/ProductDetail.tsx
@@ -1,39 +1,11 @@
-import { EuiSpacer } from "@elastic/eui"
-import {
-  Detail,
-  DetailHeader,
-  EditButton,
-  NumberValue,
-  Row,
-  TextAreaValue,
-  TextValue,
-} from "@react-mool/eui"
+import { Detail, DetailHeader } from "@react-mool/eui"
+import { Form } from "./Form"
 
 export const ProductDetail = () => {
   return (
     <Detail>
       <DetailHeader />
-      <div style={{ maxWidth: 900 }}>
-        <Row>
-          <TextValue name="reference" />
-          <TextValue name="category_id" />
-        </Row>
-        <Row spacer>
-          <NumberValue name="price" />
-          <NumberValue name="stock" />
-        </Row>
-        <Row spacer>
-          <TextValue name="image" />
-          <TextValue name="thumbnail" />
-        </Row>
-        <Row spacer>
-          <NumberValue name="height" />
-          <NumberValue name="width" />
-        </Row>
-        <TextAreaValue name="description" fullWidth />
-      </div>
-      <EuiSpacer />
-      <EditButton />
+      <Form />
     </Detail>
   )
 }
