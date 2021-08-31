4f21b947676831808946877a3cc2bf710df311b2
Filippo Dossena <filippo.dossena@mozart.it>
2021-08-31 16:07:51 +0200

    Redirect to home when authenticated user goes to login page

--- packages/eui/src/login/Login.tsx
+++ packages/eui/src/login/Login.tsx
@@ -1,9 +1,17 @@
+import { useIsAuthenticated } from "@react-mool/core"
+import { Redirect } from "react-router"
 import { LoginForm, LoginFormProps } from "./LoginForm"
 import { LoginLayout } from "./LoginLayout"
 
 export type LoginProps = LoginFormProps
 
 export const Login = (props: LoginProps) => {
+  const isAuthenticated = useIsAuthenticated()
+
+  if (isAuthenticated) {
+    return <Redirect to="/" />
+  }
+
   return (
     <LoginLayout>
       <LoginForm {...props} />
