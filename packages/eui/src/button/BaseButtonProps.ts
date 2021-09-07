0f4e7e40e54115e50fd9f918afa903f822552002
Filippo Dossena <filippo.dossena@mozart.it>
2021-09-07 18:30:17 +0200

    Add `href` and `onClick` props to BaseButtonProps

--- packages/eui/src/button/BaseButtonProps.ts
+++ packages/eui/src/button/BaseButtonProps.ts
@@ -1,7 +1,13 @@
 import { EuiButtonIconProps, EuiButtonProps, ExclusiveUnion } from "@elastic/eui"
+import { MouseEventHandler } from "react"
+
+type CommonProps = {
+  href?: string
+  onClick?: MouseEventHandler<HTMLAnchorElement | HTMLButtonElement>
+}
 
 type ButtonProps = Partial<EuiButtonProps>
 
 type ButtonAsIconProps = Partial<EuiButtonIconProps> & { asIcon: true }
 
-export type BaseButtonProps = ExclusiveUnion<ButtonProps, ButtonAsIconProps>
+export type BaseButtonProps = CommonProps & ExclusiveUnion<ButtonProps, ButtonAsIconProps>
