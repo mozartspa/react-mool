3c98fa307cceda4bd22692ae454e5d33c9ec1ee3
Filippo Dossena <filippo.dossena@mozart.it>
2021-09-08 15:26:54 +0200

    Makes menu item active when url belongs to it

--- packages/eui/src/layout/Menu.tsx
+++ packages/eui/src/layout/Menu.tsx
@@ -4,8 +4,13 @@ import {
   useResourceDefinitionList,
   useTranslate,
 } from "@react-mool/core"
+import { useLocation } from "react-router"
 import { useLinkProps } from "../helpers/useLinkProps"
 import { t } from "../i18n"
+
+function isPartialMatch(current: string, target: string) {
+  return current === target || current.startsWith(`${target}/`)
+}
 
 export type MenuProps = {
   hasDashboard?: boolean
@@ -18,18 +23,21 @@ export const Menu = (props: MenuProps) => {
   const resources = useResourceDefinitionList()
   const getResourceLabel = useGetResourceLabel()
   const translate = useTranslate()
+  const location = useLocation()
 
   const resourceItems: EuiListGroupItemProps[] = resources
     .filter((def) => def.list != null)
     .map((def) => ({
       label: getResourceLabel(def.name, 2),
       iconType: def.icon || "list",
+      isActive: isPartialMatch(location.pathname, `/${def.name}`),
       ...getLinkProps(`/${def.name}`),
     }))
 
   const dashboardItem: EuiListGroupItemProps = {
     label: translate(t.eui.layout.dashboard),
     iconType: "dashboardApp",
+    isActive: location.pathname === "/",
     ...getLinkProps("/"),
   }
 
