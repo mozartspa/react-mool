2e40765ec29f04d51f45af17585bed0188e3c536
Filippo Dossena <filippo.dossena@gmail.com>
2021-09-29 12:33:34 +0200

    Add `@react-mool/i18n-it` package

---
+++ packages/i18n-it/README.md
@@ -0,0 +1,1 @@
+# @react-mool/i18n-it
---
+++ packages/i18n-it/src/index.ts
@@ -0,0 +1,80 @@
+import { CoreMessages } from "@react-mool/core"
+import { EuiMessages } from "@react-mool/eui"
+
+const coreMessages: CoreMessages = {
+  core: {
+    crud: {
+      created: "Elemento creato",
+      updated: "Elemento modificato",
+      deleted: "Elemento eliminato",
+      invalid_form:
+        "Uno o più campi non sono corretti. Verifica i dati inseriti e riprova.",
+      item_not_found: "Elemento non trovato",
+    },
+    i18n: {
+      change_locale_error: "Impossibile caricare le traduzioni per la lingua specificata",
+    },
+  },
+}
+
+const euiMessages: EuiMessages = {
+  eui: {
+    login: {
+      title: "Log in",
+      username_label: "Username",
+      password_label: "Password",
+      invalid_credentials: "Credenziali non valide",
+      login_btn: "Log in",
+    },
+    logout: {
+      account_menu: "Account menu",
+      logout_btn: "Log out",
+    },
+    layout: {
+      dock_navigation: "Blocca",
+      undock_navigation: "Sblocca",
+      dashboard: "Dashboard",
+    },
+    action: {
+      create: "Crea",
+      view: "Vedi",
+      edit: "Modifica",
+      save: "Salva",
+      delete: "Elimina",
+      retry: "Riprova",
+      goBack: "Torna indietro",
+    },
+    bulk: {
+      actions: "Azioni di gruppo",
+      delete_confirm: "Eliminare 1 elemento? |||| Eliminare %{smart_count} elementi?",
+      deleted_items: "Elemento eliminato |||| %{smart_count} elementi eliminati",
+    },
+    grid: {
+      showing: "Mostrando %{count} of %{total}",
+      selected: "Selezionati",
+      no_items: "Nessun elemento da visualizzare",
+    },
+    filter: {
+      add_filter: "Aggiungi filtro",
+      remove_filter: "Rimuovi filtro",
+      reset_filters: "Reimposta tutti i filtri",
+    },
+    confirm: {
+      cancel: "Annulla",
+      ok: "OK",
+    },
+    error: {
+      general: "Ooops, qualcosa è andato storto",
+      notfound_title: "Non trovato",
+      notfound_message:
+        "Hai digitato un URL non corretto, o hai seguito un link sbagliato...",
+    },
+  },
+}
+
+const messages = {
+  ...coreMessages,
+  ...euiMessages,
+}
+
+export default messages
