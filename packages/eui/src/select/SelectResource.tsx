648ea396ef219caabab9450cf47e2506d6bd6f97
Filippo Dossena <filippo.dossena@gmail.com>
2021-10-13 18:25:12 +0200

    Add <SelectResource/>

---
+++ packages/eui/src/select/SelectResource.tsx
@@ -0,0 +1,66 @@
+import {
+  GetListParams,
+  useGetList,
+  useGetRecordId,
+  useGetRecordName,
+} from "@react-mool/core"
+import { useMemo } from "react"
+import { Select, SelectOption, SelectProps } from "../select"
+
+export type SelectResourceProps<TRecord = any, TFilter = any> = Omit<
+  SelectProps,
+  "options"
+> & {
+  resource: string
+  filter?: TFilter
+  sortField?: GetListParams["sortField"]
+  sortOrder?: GetListParams["sortOrder"]
+  maxSize?: number
+  toOptions?: (records: TRecord[]) => SelectOption[]
+}
+
+export const SelectResource = <TRecord extends any, TFilter>(
+  props: SelectResourceProps<TRecord, TFilter>
+) => {
+  const {
+    resource,
+    filter,
+    sortField,
+    sortOrder,
+    maxSize = 2000,
+    toOptions,
+    ...rest
+  } = props
+
+  const getId = useGetRecordId(resource)
+  const getName = useGetRecordName(resource)
+
+  const query = useGetList<TRecord, TFilter>(
+    { page: 1, pageSize: maxSize, filter, sortField, sortOrder },
+    {
+      resource,
+    }
+  )
+
+  const options = useMemo(() => {
+    if (!query.data) {
+      return []
+    }
+
+    if (toOptions) {
+      return toOptions(query.data.items)
+    }
+
+    return query.data.items.map((item) => {
+      const opt: SelectOption = {
+        value: getId(item),
+        label: getName(item),
+      }
+      return opt
+    })
+  }, [query.data, toOptions, getId, getName])
+
+  return (
+    <Select {...rest} options={options} isLoading={rest.isLoading ?? query.isLoading} />
+  )
+}
