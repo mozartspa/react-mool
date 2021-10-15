bd787212ca7ffdb73ad959b433f47ba1be831d01
Filippo Dossena <filippo.dossena@gmail.com>
2021-10-15 13:11:48 +0200

    Add `loadingView` and `errorView` to <GetOne/> and <GetMany/> + remove `react-query` props

--- packages/core/src/dataProvider/GetOne.tsx
+++ packages/core/src/dataProvider/GetOne.tsx
@@ -1,21 +1,32 @@
 import { ReactNode } from "react"
 import { UseQueryResult } from "react-query"
 import { RecordID } from "./dataProvider"
-import { useGetOne, UseGetOneOptions } from "./queries"
+import { useGetOne } from "./queries"
 
-export type GetOneProps<TRecord = any> = UseGetOneOptions<TRecord> & {
+export type GetOneProps<TRecord = any> = {
   id: RecordID
+  resource?: string
   children: (
     record: TRecord | undefined,
     meta: UseQueryResult<TRecord, unknown>
   ) => ReactNode
+  loadingView?: ReactNode
+  errorView?: ReactNode
 }
 
 export const GetOne = <TRecord extends any>(props: GetOneProps<TRecord>) => {
-  const { id, children, ...options } = props
-  const query = useGetOne(id, options)
+  const { id, resource, children, loadingView, errorView } = props
+  const query = useGetOne(id, { resource })
 
   const record = query.data
+
+  if (loadingView && query.isLoading) {
+    return <>{loadingView}</>
+  }
+
+  if (errorView && query.isError) {
+    return <>{errorView}</>
+  }
 
   return <>{children(record, query)}</>
 }
