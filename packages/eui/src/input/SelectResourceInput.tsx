2856a5e4af5176ea5e6484c6283af02a1f9d4351
Filippo Dossena <filippo.dossena@gmail.com>
2021-10-20 16:39:22 +0200

    Add `selectLabel` to <SelectResourceInput/>

--- packages/eui/src/input/SelectResourceInput.tsx
+++ packages/eui/src/input/SelectResourceInput.tsx
@@ -1,3 +1,6 @@
+import { useTranslate } from "@react-mool/core"
+import { ReactNode } from "react"
+import { useGetResourceFieldLabel } from "../helpers"
 import { SelectResource, SelectResourceProps } from "../select/SelectResource"
 import { noFormat } from "./helpers/noFormat"
 import { Input, InputProps } from "./Input"
@@ -5,13 +8,22 @@ import { Input, InputProps } from "./Input"
 const defaultEmptyValue = () => null
 
 export type SelectResourceInputProps<TRecord = any, TFilter = any> = InputProps &
-  Omit<SelectResourceProps<TRecord, TFilter>, "value" | "onChange" | "onBlur">
+  Omit<SelectResourceProps<TRecord, TFilter>, "value" | "onChange" | "onBlur"> & {
+    selectLabel?: ReactNode
+  }
 
 export const SelectResourceInput = <TRecord extends any, TFilter>(
   props: SelectResourceInputProps<TRecord, TFilter>
 ) => {
+  const { selectLabel, ...rest } = props
+
+  const getFieldLabel = useGetResourceFieldLabel()
+  const translate = useTranslate()
+
+  const label = selectLabel ?? props.label ?? getFieldLabel(props.name)
+
   return (
-    <Input {...props} format={props.format ?? noFormat}>
+    <Input {...rest} format={props.format ?? noFormat}>
       {(field, inputProps) => (
         <SelectResource
           value={field.input.value}
@@ -20,6 +32,7 @@ export const SelectResourceInput = <TRecord extends any, TFilter>(
           fullWidth={props.fullWidth}
           {...inputProps}
           emptyValue={inputProps.emptyValue ?? defaultEmptyValue}
+          label={translate(label)}
         />
       )}
     </Input>
