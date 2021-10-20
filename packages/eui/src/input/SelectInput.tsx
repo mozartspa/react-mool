c9ccb0cf243123fce07ae8bc073e75b00e5f8661
Filippo Dossena <filippo.dossena@gmail.com>
2021-10-20 16:41:23 +0200

    Add `selectLabel` to <SelectInput/>

--- packages/eui/src/input/SelectInput.tsx
+++ packages/eui/src/input/SelectInput.tsx
@@ -1,22 +1,38 @@
+import { ReactNode } from "react"
+import { useTranslate } from "../../../core/dist/esm"
+import { useGetResourceFieldLabel } from "../helpers"
 import { Select, SelectProps } from "../select"
 import { noFormat } from "./helpers/noFormat"
 import { Input, InputProps } from "./Input"
 
 const defaultEmptyValue = () => null
 
-export type SelectInputProps = InputProps & SelectProps
+export type SelectInputProps = InputProps &
+  SelectProps & {
+    selectLabel?: ReactNode
+  }
 
-export const SelectInput = (props: SelectInputProps) => (
-  <Input {...props} format={props.format ?? noFormat}>
-    {(field, inputProps) => (
-      <Select
-        value={field.input.value}
-        onBlur={() => field.setTouched(true)}
-        onChange={(value) => field.setValue(value)}
-        fullWidth={props.fullWidth}
-        {...inputProps}
-        emptyValue={inputProps.emptyValue ?? defaultEmptyValue}
-      />
-    )}
-  </Input>
-)
+export const SelectInput = (props: SelectInputProps) => {
+  const { selectLabel, ...rest } = props
+
+  const getFieldLabel = useGetResourceFieldLabel()
+  const translate = useTranslate()
+
+  const label = selectLabel ?? props.label ?? getFieldLabel(props.name)
+
+  return (
+    <Input {...rest} format={props.format ?? noFormat}>
+      {(field, inputProps) => (
+        <Select
+          value={field.input.value}
+          onBlur={() => field.setTouched(true)}
+          onChange={(value) => field.setValue(value)}
+          fullWidth={props.fullWidth}
+          {...inputProps}
+          emptyValue={inputProps.emptyValue ?? defaultEmptyValue}
+          label={translate(label)}
+        />
+      )}
+    </Input>
+  )
+}
