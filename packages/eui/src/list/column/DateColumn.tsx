de18ceddd3dd9b08895546c4b53f9bc9218ea7f8
Filippo Dossena <filippo.dossena@mozart.it>
2021-10-26 19:24:04 +0200

    Fix <DateColumn/> props typings

--- packages/eui/src/list/column/DateColumn.tsx
+++ packages/eui/src/list/column/DateColumn.tsx
@@ -1,7 +1,8 @@
 import { DateTime, DateTimeProps } from "../../utilities"
 import { ColumnProps } from "./Column"
 
-export type DateColumnProps = ColumnProps & DateTimeProps
+export type DateColumnProps = ColumnProps &
+  Pick<DateTimeProps, "locale" | "formatOptions">
 
 export const DateColumn = (props: DateColumnProps) => {
   const { value, locale, formatOptions } = props
