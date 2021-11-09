daac09212143064593fa64489c9453216ab56567
Filippo Dossena <filippo.dossena@mozart.it>
2021-11-09 12:17:05 +0100

    Fix `useInputValue` hook not taking into account the field scope value

--- packages/eui/src/input/useInputValue.ts
+++ packages/eui/src/input/useInputValue.ts
@@ -1,14 +1,21 @@
-import { useFormContext } from "@mozartspa/mobx-form"
+import { FieldScopeContext, useFormContext } from "@mozartspa/mobx-form"
 import { useObserveMobxValue } from "@react-mool/core"
+import React from "react"
 
 export function useInputValue<TValue = any>(name: string): TValue {
   const form = useFormContext()
+  const fieldPrefix = React.useContext(FieldScopeContext)
+
+  const fieldName = fieldPrefix + name
 
   // To avoid that the caller of `useInputValue` have to wrap
   // the component with mobx `observer` (we're accessing a mobx
   // observable value, thus we have to do it), we listen to its
   // changes with `useObserveMobxValue`.
-  const inputValue = useObserveMobxValue(() => form?.getFieldValue(name), [form, name])
+  const inputValue = useObserveMobxValue(
+    () => form?.getFieldValue(fieldName),
+    [form, fieldName]
+  )
 
   return inputValue
 }
