a39cc675ed89d1213aa8f37133210e611fd3b21f
Filippo Dossena <filippo.dossena@mozart.it>
2021-11-12 11:56:18 +0100

    Fix `requiredValidator` test function

--- packages/eui/src/input/helpers/requiredValidator.ts
+++ packages/eui/src/input/helpers/requiredValidator.ts
@@ -1,6 +1,15 @@
 import { FieldValidate } from "@mozartspa/mobx-form"
 import { t } from "../../i18n"
 
+function isEmpty(value: any) {
+  return (
+    value == null ||
+    value === "" ||
+    (Array.isArray(value) && value.length === 0) ||
+    (typeof value === "object" && Object.keys(value).length === 0)
+  )
+}
+
 export const requiredValidator: FieldValidate = (value: any) => {
-  return value == null ? t.eui.validation.required : undefined
+  return isEmpty(value) ? t.eui.validation.required : undefined
 }
