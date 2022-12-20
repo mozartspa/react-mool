3620305e0e18233d0baf355b7b83473d9a50426e
Filippo Dossena <filippo.dossena@mozart.it>
2022-12-20 11:53:04 +0100

    Add `EuiBreadcrumbsProps` to <BreadcrumbsContainer/>

--- packages/eui/src/layout/Breadcrumbs.tsx
+++ packages/eui/src/layout/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { EuiHeaderBreadcrumbs } from "@elastic/eui"
+import { EuiBreadcrumbsProps, EuiHeaderBreadcrumbs } from "@elastic/eui"
 import { EuiBreadcrumbProps } from "@elastic/eui/src/components/breadcrumbs/breadcrumb"
 import { useLinkProps } from "@react-mool/core"
 import { ReactNode } from "react"
@@ -15,7 +15,9 @@ export const BreadcrumbsItem = (props: BreadcrumbsItemProps) => {
   return <Producer data={props} />
 }
 
-export const BreadcrumbsContainer = () => {
+export type BreadcrumbsContainerProps = Omit<EuiBreadcrumbsProps, "breadcrumbs">
+
+export const BreadcrumbsContainer = (props: BreadcrumbsContainerProps) => {
   const linkProps = useLinkProps()
 
   return (
@@ -28,7 +30,7 @@ export const BreadcrumbsContainer = () => {
             ...(item.to ? linkProps(item.to) : {}),
           }))
 
-        return <EuiHeaderBreadcrumbs breadcrumbs={breadcrumbs} />
+        return <EuiHeaderBreadcrumbs {...props} breadcrumbs={breadcrumbs} />
       }}
     </Consumer>
   )
