ba85028f6e3840e90f0ad391b75be30ed9c80afc
Filippo Dossena <filippo.dossena@mozart.it>
2023-02-20 13:15:29 +0100

    Remove `basename` from `returnURL` of `useReturnURL` hook

--- packages/core/src/admin/index.ts
+++ packages/core/src/admin/index.ts
@@ -1,2 +1,3 @@
 export * from "./AdminContext"
 export * from "./AdminRouter"
+export * from "./BasenameContext"
--- packages/core/src/helpers/useReturnURL.ts
+++ packages/core/src/helpers/useReturnURL.ts
@@ -1,14 +1,30 @@
 import { LocationDescriptor, parsePath } from "history"
 import { useLocation } from "react-router"
+import { useBasename } from "../admin"
+
+function removeBasenameFromPath(basename: string | undefined, path: string) {
+  if (basename == null) {
+    return path
+  }
+  const token = `/${basename}/`
+  if (path.startsWith(token)) {
+    return "/" + path.slice(token.length)
+  } else {
+    return path
+  }
+}
 
 export function useReturnURL() {
   const location = useLocation()
+  const basename = useBasename()
 
   function addReturnURL(url: string, returnUrl?: string): LocationDescriptor {
     const location = parsePath(url)
 
     if (!returnUrl) {
-      returnUrl = window.location.pathname + window.location.search
+      returnUrl =
+        removeBasenameFromPath(basename, window.location.pathname) +
+        window.location.search
     }
 
     return {
