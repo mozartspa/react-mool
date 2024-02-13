7dd3276a9ffc6717ef23b1065bd0ac3fcfab0858
Filippo Dossena <filippo.dossena@mozart.it>
2024-02-13 15:39:59 +0100

    Add support to unknown values in <ComboBoxInput/>

--- packages/eui/src/input/ComboBoxInput.tsx
+++ packages/eui/src/input/ComboBoxInput.tsx
@@ -1,4 +1,5 @@
 import { EuiComboBox, EuiComboBoxOptionOption, EuiComboBoxProps } from "@elastic/eui"
+import { ReactNode, useEffect, useState } from "react"
 import { noFormat } from "./helpers/noFormat"
 import { Input, InputProps } from "./Input"
 
@@ -20,24 +21,62 @@ type ComboBoxProps = Pick<
   | "delimiter"
 >
 
-export type ComboBoxInputProps = InputProps & ComboBoxProps
+type MoreProps = {
+  keepUnknownValues?: boolean
+  onUnknownValues?: (values: any[]) => void
+  renderUnknownValuesHint?: (values: any[]) => ReactNode
+}
+
+export type ComboBoxInputProps = InputProps & ComboBoxProps & MoreProps
+
+const EMPTY_ARRAY: any[] = []
 
 export const ComboBoxInput = (props: ComboBoxInputProps) => {
-  const { options = [], format = noFormat } = props
+  const {
+    options = [],
+    format = noFormat,
+    keepUnknownValues = false,
+    onUnknownValues,
+    renderUnknownValuesHint,
+    ...restProps
+  } = props
+
+  const [unknownValuesState, setUnknownValues] = useState<any[]>([])
+
+  const helpText = renderUnknownValuesHint ? (
+    <>
+      {restProps.helpText}
+      {renderUnknownValuesHint(unknownValuesState)}
+    </>
+  ) : (
+    restProps.helpText
+  )
 
   return (
-    <Input {...props} format={format}>
+    <Input {...restProps} format={format} helpText={helpText}>
       {(field, inputProps) => {
-        const value = Array.isArray(field.input.value) ? field.input.value : []
+        const value = Array.isArray(field.input.value) ? field.input.value : EMPTY_ARRAY
+        const unknownValues = value.filter(
+          (val) => options.find((o) => val === o.value ?? o.label) === undefined
+        )
         const selectedOptions = options.filter((o) => value.includes(o.value ?? o.label))
         const handleChange = (opts: EuiComboBoxOptionOption[]) => {
-          const newValue = opts.map((o) => o.value ?? o.label)
+          let newValue = opts.map((o) => o.value ?? o.label)
+          if (keepUnknownValues) {
+            newValue = [...newValue, ...unknownValues]
+          }
           field.setValue(newValue)
         }
+
+        useEffect(() => {
+          setUnknownValues(unknownValues)
+          onUnknownValues?.(unknownValues)
+        }, [JSON.stringify(unknownValues)])
 
         return (
           <EuiComboBox
             {...inputProps}
+            options={options}
             selectedOptions={selectedOptions}
             onBlur={field.input.onBlur}
             onChange={handleChange}
